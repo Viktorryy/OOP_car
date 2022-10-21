5afe96157260ec1f0d8110266d83bbe1d212d5b0
Viktorryy <viktory_o@rambler.ru>
2022-10-21 13:19:35 +0400

    Решение

---
+++ src/Cars.java
@@ -0,0 +1,104 @@
+import java.util.Objects;
+
+public class Cars {
+    private String brand;
+    private String model;
+    private float volumeEngine;
+    private String colorBody;
+    private int yearProduction;
+    private String productionCountry;
+
+    public Cars(String brand, String model, float volumeEngine, String colorBody, int yearProduction, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.volumeEngine = volumeEngine;
+        this.colorBody = colorBody;
+        this.yearProduction = yearProduction;
+        this.productionCountry = productionCountry;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getVolumeEngine() {
+        return volumeEngine;
+    }
+
+    public void setVolumeEngine(float volumeEngine) {
+        this.volumeEngine = volumeEngine;
+    }
+
+    public String getColorBody() {
+        return colorBody;
+    }
+
+    public void setColorBody(String colorBody) {
+        this.colorBody = colorBody;
+    }
+
+    public int getYearProduction() {
+        return yearProduction;
+    }
+
+    public void setYearProduction(int yearProduction) {
+        this.yearProduction = yearProduction;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public void setProductionCountry(String productionCountry) {
+        this.productionCountry = productionCountry;
+    }
+
+
+    public static String toString(Cars cars) {
+        return "Cars{" +
+                "brand='" + cars.getBrand() + '\'' +
+                ", model='" + cars.getModel() + '\'' +
+                ", volumeEngine=" + cars.getVolumeEngine() +
+                ", colorBody='" + cars.getColorBody() + '\'' +
+                ", yearProduction=" + cars.getYearProduction() +
+                ", productionCountry='" + cars.getProductionCountry() + '\'' +
+                '}';
+    }
+    public static String infoCar(Cars cars) {
+        return
+                "brand=" + cars.getBrand() +
+                ", model=" + cars.getModel() +
+                ", volumeEngine=" + cars.getVolumeEngine() +
+                ", colorBody=" + cars.getColorBody() +
+                ", yearProduction=" + cars.getYearProduction() +
+                ", productionCountry=" + cars.getProductionCountry() ;
+
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cars)) return false;
+        Cars cars = (Cars) o;
+        return Float.compare(cars.getVolumeEngine(), getVolumeEngine()) == 0 && getYearProduction() == cars.getYearProduction() && getBrand().equals(cars.getBrand()) && getModel().equals(cars.getModel()) && getColorBody().equals(cars.getColorBody()) && getProductionCountry().equals(cars.getProductionCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand(), getModel(), getVolumeEngine(), getColorBody(), getYearProduction(), getProductionCountry());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cars[] car = new Cars[5];
+        car[0] = new Cars("Lada"," Granta", 1.7f ,"желтый", 2015 ,"Россия");
+        car[1] = new Cars("Audi", "A8 50 L TDI quattro", 3.0f , "черный" , 2020 , "Германия");
+        car[2] = new Cars("BMW", "Z8",3.0f , "черный" ,2021 , "Германия");
+        car[3] = new Cars("Kia", "Sportage 4-го поколения", 2.4f , "красный", 2018 , "Южная Корея");
+        car[4] = new Cars("Hyundai", "Avante",1.6f  ,"оранжевый", 2016, "Южная Корея");
+
+        for (Cars i: car) {
+            System.out.println(Cars.infoCar(i));
+        }
+
     }
 }
